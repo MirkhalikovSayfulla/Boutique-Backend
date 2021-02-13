1d3532e2fe45ab66b0463f25034494022d6d3cb8
MirkhalikovSayfulla <mirxoliqov.sayfulla@gmail.com>
2021-02-13 22:12:59 +0000

    change test_models.py file and completed

--- products/tests/test_models.py
+++ products/tests/test_models.py
@@ -1,12 +1,19 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from products.models import (
-    Customer,
-    Subscribe,
+    Brand,
     Category,
+    Coupon,
+    Customer,
+    Order,
+    Product,
+    ProductView,
+    Subscribe,
     Type,
-    Brand
+    OrderItem,
+    Wishlist
 )
 
 
@@ -14,6 +21,39 @@ class TestModel(TestCase):
 
     def setUp(self):
         User.objects.create_user('admin', 'admin@gmail.com', 'admin')
+        self.brand = Brand.objects.create(
+            name='FILA'
+        )
+        self.category = Category.objects.create(
+            name='Watches'
+        )
+        self.type = Type.objects.create(
+            name='Electronics'
+        )
+        self.product1 = Product.objects.create(
+            name='product1',
+            price=30.21,
+            image='/media/products/product-1.jpg',
+            about='very nice product',
+            sku=99999,
+            brand=self.brand,
+            category=self.category,
+            description='<h2>All Ok</h2>',
+            status='info',
+            stars=4,
+            completed=False
+        )
+        self.product1.type.add(self.type)
+        self.coupon = Coupon.objects.create(
+            code='ABC',
+            discount=20
+        )
+        customer = Customer.objects.get(id=1)
+        self.order = Order.objects.create(
+            customer=customer,
+            coupon=self.coupon,
+            complete=False
+        )
 
     def test_customer_model(self):
         self.assertTrue(Customer.objects.all())
@@ -26,22 +66,46 @@ class TestModel(TestCase):
         self.assertEquals(s.name, 'admin@gmail.com')
 
     def test_category_model(self):
-        Category.objects.create(
-            name='Watches'
-        )
         c = Category.objects.get(id=1)
         self.assertEquals(c.name, 'Watches')
 
     def test_type_model(self):
-        Type.objects.create(
-            name='Electronics'
-        )
         t = Type.objects.get(id=1)
         self.assertEquals(t.name, 'Electronics')
 
-    def test_type_model(self):
-        Brand.objects.create(
-            name='FILA'
-        )
+    def test_brand_model(self):
         b = Brand.objects.get(id=1)
         self.assertEquals(b.name, 'FILA')
+
+    def test_product_model(self):
+        self.assertTrue(self.product1)
+
+    def test_product_view_model(self):
+        self.assertFalse(ProductView.objects.all())
+        ProductView.objects.create(
+            product=self.product1,
+            image='/media/products/product-1.jpg'
+        )
+        self.assertTrue(ProductView.objects.all())
+
+    def test_coupon_model(self):
+        self.assertTrue(Coupon.objects.all())
+
+    def test_order_model(self):
+        self.assertTrue(Order.objects.all())
+
+    def test_order_item_model(self):
+        OrderItem.objects.create(
+            product=self.product1,
+            order=self.order,
+            quantity=2,
+            date_added=timezone.now()
+        )
+        self.assertTrue(OrderItem.objects.all())
+
+    def test_wishlist_model(self):
+        Wishlist.objects.create(
+            product=self.product1,
+            order=self.order
+        )
+        self.assertTrue(Wishlist.objects.all())
